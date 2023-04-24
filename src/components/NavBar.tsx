e963983c0417e91209bab73111646b8ef40b1374
Himo <himo@Himos-MacBook-Pro.local>
2023-04-24 06:03:02 -0700

    Modify - Navbar Layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { Box, HStack, Image } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 import logo from '../assets/logo.webp'
@@ -7,11 +7,13 @@ import SearchInput from './SearchInput'
 
 const NavBar = () => {
   return (
-    <HStack padding="10px">
+    <HStack padding="10px" justifyContent="space-between" spacing={5}>
       <Link to="/">
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
-      <SearchInput />
+      <Box width="88%">
+        <SearchInput />
+      </Box>
       <ColorModeSwitch />
     </HStack>
   )
